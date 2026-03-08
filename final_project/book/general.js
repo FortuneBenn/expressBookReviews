ecf1f7f39e24ad0847fb61b9a9a50f2080bd710c
FortuneBenn <fortuneben69@gmail.com>
2026-03-08 17:47:57 +0200

    Completed IBM Express Book Review Final Project

--- final_project/book/general.js
+++ final_project/book/general.js
@@ -2,61 +2,53 @@ const axios = require("axios");
 
 const BASE_URL = "http://localhost:5000";
 
-/*
-Retrieve all books using async/await
-*/
+/* Get all books using async/await */
 async function getAllBooks() {
-  try {
-    const response = await axios.get(`${BASE_URL}/`);
-    console.log("All Books:");
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get(`${BASE_URL}/`);
+        console.log("All Books:");
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-/*
-Search by ISBN using Promises
-*/
+/* Get book by ISBN using Promises */
 function getBookByISBN(isbn) {
-  axios.get(`${BASE_URL}/isbn/${isbn}`)
+    axios.get(`${BASE_URL}/isbn/${isbn}`)
     .then(response => {
-      console.log("Book by ISBN:");
-      console.log(response.data);
+        console.log("Book by ISBN:");
+        console.log(response.data);
     })
     .catch(error => {
-      console.error(error);
+        console.error(error);
     });
 }
 
-/*
-Search by Author using async/await
-*/
+/* Get books by author using async/await */
 async function getBooksByAuthor(author) {
-  try {
-    const response = await axios.get(`${BASE_URL}/author/${author}`);
-    console.log("Books by Author:");
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get(`${BASE_URL}/author/${author}`);
+        console.log("Books by Author:");
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-/*
-Search by Title using Promises
-*/
+/* Get books by title using Promises */
 function getBooksByTitle(title) {
-  axios.get(`${BASE_URL}/title/${title}`)
+    axios.get(`${BASE_URL}/title/${title}`)
     .then(response => {
-      console.log("Books by Title:");
-      console.log(response.data);
+        console.log("Books by Title:");
+        console.log(response.data);
     })
     .catch(error => {
-      console.error(error);
+        console.error(error);
     });
 }
 
-/* Run tests */
+/* Run all functions */
 getAllBooks();
 getBookByISBN("9781593275846");
 getBooksByAuthor("Richard Silverman");
